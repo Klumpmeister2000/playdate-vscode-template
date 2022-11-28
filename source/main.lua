86d633ae23177958b7440d6d098118bc536e8210
Klumpmeister2000 <christian@klumpmeister.net>
2022-11-28 13:28:14 -0500

    syntax and collisions

--- source/main.lua
+++ source/main.lua
@@ -12,15 +12,31 @@ local playerSpeed = 4
 local playTimer = nil
 local playTime = 30 * 1000
 
+local coinSprite = nil
+
 local function resetTimer()
     playTimer = playdate.timer.new(playTime, playTime, 0, playdate.easingFunctions.linear)
 end
 
+local function MoveCoin()
+    local randX = math.random(40,360)
+    local randY = math.random(40,200)
+    coinSprite:moveTo(randX, randY)
+end
+
 local function initalize()
+    math.randomseed(playdate.getSecondsSinceEpoch())
     local playerImage = gfx.image.new("images/player.png")
     playerSprite = gfx.sprite.new(playerImage)
     playerSprite:moveTo(200,120)
+    playerSprite:setCollideRect(0, 0, playerSprite:getSize())
     playerSprite:add()
+
+    local coinImage = gfx.image.new("images/coin.png")
+    coinSprite = gfx.sprite.new(coinImage)
+    moveCoin()
+    coinSprite:setCollideRect(0, 0, coinSprite:getSize())
+    coinSprite:add()
 
     local backgroundImage = gfx.image.new("images/background.png")
     gfx.sprite.setBackgroundDrawingCallback(
@@ -40,6 +56,7 @@ function playdate.update()
     if playTimer.value == 0 then
         if playdate.buttonJustPressed(playdate.kButtonA) then
             resetTimer()
+            moveCoin()
         end
     else
         if playdate.buttonIsPressed(playdate.kButtonUp) then
@@ -54,6 +71,10 @@ function playdate.update()
         if playdate.buttonIsPressed(playdate.kButtonLeft) then
         playerSprite:moveBy(-playerSpeed, 0)
         end
+
+        local collisions = coinSprite:overlappingSprites()
+        if #collisions >= 1 then
+            moveCoin()
 end
     
 playdate.timer.updateTimers()
