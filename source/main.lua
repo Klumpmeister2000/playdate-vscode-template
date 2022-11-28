f7d9f7de0f1bf582d25f40d3c5eded899f76b916
Klumpmeister2000 <christian@klumpmeister.net>
2022-11-28 13:16:46 -0500

    added movement and timers

--- source/main.lua
+++ source/main.lua
@@ -7,6 +7,15 @@ local gfx <const> = playdate.graphics
 
 local playerSprite = nil
 
+local playerSpeed = 4
+
+local playTimer = nil
+local playTime = 30 * 1000
+
+local function resetTimer()
+    playTimer = playdate.timer.new(playTime, playTime, 0, playdate.easingFunctions.linear)
+end
+
 local function initalize()
     local playerImage = gfx.image.new("images/player.png")
     playerSprite = gfx.sprite.new(playerImage)
@@ -21,10 +30,34 @@ local function initalize()
             gfx.clearClipRect()
         end
     )
+
+    resetTimer()
 end
 
 initalize()
 
 function playdate.update()
+    if playTimer.value == 0 then
+        if playdate.buttonJustPressed(playdate.kButtonA) then
+            resetTimer()
+        end
+    else
+        if playdate.buttonIsPressed(playdate.kButtonUp) then
+        playerSprite:moveBy(0, -playerSpeed)
+        end
+        if playdate.buttonIsPressed(playdate.kButtonRight) then
+        playerSprite:moveBy(playerSpeed, 0)
+        end
+        if playdate.buttonIsPressed(playdate.kButtonDown) then
+        playerSprite:moveBy(0, playerSpeed)
+        end
+        if playdate.buttonIsPressed(playdate.kButtonLeft) then
+        playerSprite:moveBy(-playerSpeed, 0)
+        end
+end
+    
+playdate.timer.updateTimers()
     gfx.sprite.update()
+
+    gfx.drawText("Time:" .. math.ceil(playTimer.value/1000), 5, 5)
 end
