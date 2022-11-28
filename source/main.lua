6b1c85f30fd8e48c02227a2e5f60c83cfc01ed3c
Klumpmeister2000 <christian@klumpmeister.net>
2022-11-28 13:00:01 -0500

    added images, background to main lua

--- source/main.lua
+++ source/main.lua
@@ -7,14 +7,23 @@ local gfx <const> = playdate.graphics
 
 local playerSprite = nil
 
-local function initialize()
-    local playerImage = gfx.image.new("images/player")
+local function initalize()
+    local playerImage = gfx.image.new("images/player.png")
     playerSprite = gfx.sprite.new(playerImage)
-    playerSprite:moveTo(200, 120)
+    playerSprite:moveTo(200,120)
     playerSprite:add()
+
+    local backgroundImage = gfx.image.new("images/background.png")
+    gfx.sprite.setBackgroundDrawingCallback(
+        function(x, y, width, height)
+            gfx.setClipRect(x, y, width, height)
+            backgroundImage:draw(0,0)
+            gfx.clearClipRect()
+        end
+    )
 end
 
-initialize()
+initalize()
 
 function playdate.update()
     gfx.sprite.update()
